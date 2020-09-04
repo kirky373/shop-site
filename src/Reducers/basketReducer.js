d873f4a96b3c36792e534544062442b560f2bc2f
kirky373 <callum.kirkwood@outlook.com>
2020-09-04 15:09:44 +0100

    Update basketReducer.js
    Refactored the Reducer

--- src/Reducers/basketReducer.js
+++ src/Reducers/basketReducer.js
@@ -1,3 +1,4 @@
+import React, { Component } from "react";
 import {
   ADD_PRODUCT_BASKET,
   GET_NUMBERS_BASKET,
@@ -37,93 +38,120 @@ const initialState = {
     },
   },
 };
+class basketReducer extends Component {
+  constructor(props) {
+    super(props);
+    console.log("CALLED");
+  }
+  componentDidMount() {
+    console.log("MOUNTED");
+  }
+}
+let productSelected = "";
+let newCartCost = 0;
+let newBasketNumbers = 0;
+
+function addProductBasket(state, action) {
+  newCartCost = 0;
+  productSelected = { ...state.products[action.payload] };
+  if (productSelected.numbers === productSelected.stock) {
+    productSelected.numbers = productSelected.stock;
+    newBasketNumbers = state.basketNumbers;
+  } else {
+    productSelected.numbers += 1;
+    newBasketNumbers = state.basketNumbers + 1;
+    newCartCost = state.cartCost + productSelected.price;
+  }
+  productSelected.inCart = true;
+  console.log(productSelected);
+
+  return {
+    ...state,
+    basketNumbers: newBasketNumbers,
+    cartCost: newCartCost,
+    products: {
+      ...state.products,
+      [action.payload]: productSelected,
+    },
+  };
+}
+
+function increaseQuantity(state, action) {
+  productSelected = { ...state.products[action.payload] };
+  newCartCost = 0;
+  newBasketNumbers = 0;
+  if (productSelected.numbers === productSelected.stock) {
+    productSelected.numbers = productSelected.stock;
+    newCartCost = state.cartCost;
+    newBasketNumbers = state.basketNumbers;
+  } else {
+    productSelected.numbers += 1;
+    newCartCost = state.cartCost + state.products[action.payload].price;
+    newBasketNumbers = state.basketNumbers + 1;
+  }
+  return {
+    ...state,
+    basketNumbers: newBasketNumbers,
+    cartCost: newCartCost,
+    products: {
+      ...state.products,
+      [action.payload]: productSelected,
+    },
+  };
+}
+
+function decreaseQuantity(state, action) {
+  productSelected = { ...state.products[action.payload] };
+  newCartCost = 0;
+  newBasketNumbers = 0;
+  if (productSelected.numbers === 0) {
+    productSelected.numbers = 0;
+    newCartCost = state.cartCost;
+    newBasketNumbers = state.basketNumbers;
+  } else {
+    productSelected.numbers -= 1;
+    newCartCost = state.cartCost - state.products[action.payload].price;
+    newBasketNumbers = state.basketNumbers - 1;
+  }
+  return {
+    ...state,
+    basketNumbers: newBasketNumbers,
+    cartCost: newCartCost,
+    products: {
+      ...state.products,
+      [action.payload]: productSelected,
+    },
+  };
+}
+
+function clearProduct(state, action) {
+  productSelected = { ...state.products[action.payload] };
+  let numbersBackup = productSelected.numbers;
+  productSelected.numbers = 0;
+  productSelected.inCart = false;
+  return {
+    ...state,
+    basketNumbers: state.basketNumbers - numbersBackup,
+    cartCost: state.cartCost - numbersBackup * productSelected.price,
+    products: {
+      ...state.products,
+      [action.payload]: productSelected,
+    },
+  };
+}
 
 export default (state = initialState, action) => {
-  let productSelected = "";
-  let newCartCost = 0;
-  let newBasketNumbers = 0;
   switch (action.type) {
     case ADD_PRODUCT_BASKET:
-      productSelected = { ...state.products[action.payload] };
-      if (productSelected.numbers === productSelected.stock) {
-        productSelected.numbers = productSelected.stock;
-        newBasketNumbers = state.basketNumbers;
-      } else {
-        productSelected.numbers += 1;
-        newBasketNumbers = state.basketNumbers + 1;
-      }
-      productSelected.inCart = true;
-      console.log(productSelected);
-
-      return {
-        ...state,
-        basketNumbers: newBasketNumbers,
-        cartCost: newCartCost,
-        products: {
-          ...state.products,
-          [action.payload]: productSelected,
-        },
-      };
+      return addProductBasket(state, action);
     case GET_NUMBERS_BASKET:
       return { ...state };
     case INCREASE_QUANTITY:
-      productSelected = { ...state.products[action.payload] };
-      newCartCost = 0;
-      newBasketNumbers = 0;
-      if (productSelected.numbers === productSelected.stock) {
-        productSelected.numbers = productSelected.stock;
-        newCartCost = state.cartCost;
-        newBasketNumbers = state.basketNumbers;
-      } else {
-        productSelected.numbers += 1;
-        newCartCost = state.cartCost + state.products[action.payload].price;
-        newBasketNumbers = state.basketNumbers + 1;
-      }
-      return {
-        ...state,
-        basketNumbers: newBasketNumbers,
-        cartCost: newCartCost,
-        products: {
-          ...state.products,
-          [action.payload]: productSelected,
-        },
-      };
+      return increaseQuantity(state, action);
     case DECREASE_QUANTITY:
-      productSelected = { ...state.products[action.payload] };
-      newCartCost = 0;
-      newBasketNumbers = 0;
-      if (productSelected.numbers === 0) {
-        productSelected.numbers = 0;
-        newCartCost = state.cartCost;
-        newBasketNumbers = state.basketNumbers;
-      } else {
-        productSelected.numbers -= 1;
-        newCartCost = state.cartCost - state.products[action.payload].price;
-        newBasketNumbers = state.basketNumbers - 1;
-      }
-      return {
-        ...state,
-        basketNumbers: newBasketNumbers,
-        cartCost: newCartCost,
-        products: {
-          ...state.products,
-          [action.payload]: productSelected,
-        },
-      };
+      return decreaseQuantity(state, action);
     case CLEAR_PRODUCT:
-      productSelected = { ...state.products[action.payload] };
-      let numbersBackup = productSelected.numbers;
-      productSelected.numbers = 0;
-      productSelected.inCart = false;
-      return {
-        ...state,
-        basketNumbers: state.basketNumbers - numbersBackup,
-        cartCost: state.cartCost - numbersBackup * productSelected.price,
-        products: {
-          ...state.products,
-          [action.payload]: productSelected,
-        },
-      };
+      return clearProduct(state, action);
     default:
       return state;
   }
